4e994ed7a3865476fa0bea3300cd1c54b8adaf2d
RomainRullier <romainrullier@MacBook-Pro-de-Romain.local>
2023-09-21 10:02:45 +0200

    responsive + logo

--- src/components/ToolsBar.jsx
+++ src/components/ToolsBar.jsx
@@ -39,26 +39,47 @@ const Container = styled.div`
   display: flex;
   background-color: #fcf2e2;
   padding: 20px 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 10px 10px;
+  }
+  H1 {
+    font-family: 'Pacifico', cursive;
+  }
 `;
 
 const LogoContainer = styled.div`
     display: flex;
     align-items: center;
     margin-right: 20px;
+    @media (max-width: 768px) {
+      justify-content: center;
+      margin-right: 0;
+    }
 `;
 
 const ContainerToolsBar = styled.div`
     display: flex;
     align-items: center;
     margin-left: 20px;
+    @media (max-width: 768px) {
+      margin-left: 0;
+    }
 `;
 
 const ContainerTools = styled.div`
     display: flex;
     width: 100%;
     align-items: center;
-  & > * {
+    & > * {
     margin: 0 30px;
-  }
+    }
+    @media (max-width: 768px) {
+        flex-wrap: wrap;
+        justify-content: center;
+        & > * {
+          margin: 20px 25px;
+        }
+    }
 `;
 
